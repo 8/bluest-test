eea707e1d7659fb0d0511e36dbccaef116b4bc30
mdk <mdk@lostindetails.com>
2024-03-24 18:49:15 +0100

    lists services

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ async fn main() {
 
 use futures::{executor::block_on, StreamExt};
 
+// https://bitbucket.org/bluetooth-SIG/public/src/main/assigned_numbers/
+
 fn get_ble() -> () {
 
   let adapter = block_on(bluest::Adapter::default());
@@ -16,16 +18,27 @@ fn get_ble() -> () {
 
     if let Ok(_) = block_on(adapter.wait_available()) {
       println!("Adapter available");
+      println!("Querying devices...");
+      println!("");
 
       if let Ok(mut scan) = block_on(adapter.scan(&[])) {
         
-        while let Some(discovered_device) = block_on(scan.next()) {
-          println!(
-              "{:?}",
-              discovered_device.device.name().as_deref().unwrap_or("unknown")
-            );
+        while let Some(device) = block_on(scan.next()) {
+          println!("Company: {}", device.adv_data.manufacturer_data.map(|m|format!("0x{:02X}", m.company_id)).as_deref().unwrap_or("unknown"));
+          println!("Device: {}", device.device.name().as_deref().unwrap_or("unknown"));
+          println!("Connectable: {}", device.adv_data.is_connectable);
+          println!("Services: {:?}", device.adv_data.services);
+
+          if let Ok(services) = block_on(device.device.discover_services()) {
+            services.iter().for_each(|s| {
+              let uuid = s.uuid();
+              println!("uuid: {}", uuid);
+            });
           }
+
+          println!("")
         }
+      }
     }
 
   } else {
