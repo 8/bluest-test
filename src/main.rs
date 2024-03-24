e50bed90fcfe4ed8682fc723da6aeaa256cc9b5b
mdk <mdk@lostindetails.com>
2024-03-24 13:23:11 +0100

    lists devices on linux

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 
 #[tokio::main]
 async fn main() {
-  // println!("Hello, world!");
-  println!("bluest test");
+  println!("Starting bluest test");
   get_ble();
 }
 
+use futures::{executor::block_on, StreamExt};
+
 fn get_ble() -> () {
 
-  let adapter = futures::executor::block_on(bluest::Adapter::default());
+  let adapter = block_on(bluest::Adapter::default());
   
   if let Some(adapter) = adapter {
     println!("Found default adapter");
+
+    if let Ok(_) = block_on(adapter.wait_available()) {
+      println!("Adapter available");
+
+      if let Ok(mut scan) = block_on(adapter.scan(&[])) {
+        
+        while let Some(discovered_device) = block_on(scan.next()) {
+          println!(
+              "{:?}",
+              discovered_device.device.name().as_deref().unwrap_or("unknown")
+            );
+          }
+        }
+    }
+
   } else {
     println!("No default adapter found.");
   }
-
   
 }
